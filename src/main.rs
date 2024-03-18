da9effb47ee7d8811fd640e564343345d4a62e26
Niceblueman <wintheiser761@gmail.com>
2024-03-18 18:19:45 +0000

    `generate api structure using open_api_rust_generator`

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,246 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![allow(missing_docs)]
+#![allow(unused_imports)]
 
-#[macro_use] extern crate rocket;
+use chrono::{DateTime, Duration, Utc};
+use easylog::log_file::{LogFile, LogLevel};
+use easylog::log_file_config::LogFileConfig;
+use reqwest::header;
+use reqwest::*;
+use rocket::config::Config;
+use rocket::figment::Figment;
+use rocket::fs::{relative, FileServer, Options};
+use rocket::futures::FutureExt;
+use rocket::http::Method;
+use rocket::{routes, Route};
+use rocket::{catch, catchers, response, response::Responder, Response};
+use rocket::{
+    figment::providers::{Env, Format, Serialized, Toml},
+    Request,
+};
+use rocket_cors::{AllowedHeaders, AllowedMethods, AllowedOrigins, CorsOptions};
+use rocket_dyn_templates::{context, Template};
+use rocket_okapi::gen::OpenApiGenerator;
+use rocket_okapi::okapi;
+use rocket_okapi::okapi::openapi3::{MediaType, Responses};
+use rocket_okapi::okapi::schemars;
+use rocket_okapi::response::OpenApiResponderInner;
+use rocket_okapi::settings::UrlObject;
+use rocket_okapi::{openapi_get_routes, rapidoc::*, swagger_ui::*, OpenApiError};
+use serde::{Deserialize, Serialize};
+use std::env;
+use std::fmt::{format, Display};
+use std::str::FromStr;
+// --------- All different methods of implementing `OpenApiFromRequest` ------------
+// There are a few different ways of doing things.
+// And it also depend on the authentication (if any) you want to implement.
+// Here are a few different example that cover most of the use cases:
+// - No special authentication
+// - ApiKey (in http header, query or cookie)
+// - HTTP `Authorization` header (inc `basic`, `digest` and `bearer` tokens)
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication#authentication_schemes
+// - OAuth 2.0 flows (authorizationCode, implicit, password, clientCredentials)
+// - OpenID Connect
+// - Just Cookies (for just 1 route/endpoint)
+// ---------------------------------------------------------------------------------
 
-#[cfg(test)] mod tests;
+mod tools;
 
-#[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+// #[derive(Debug, PartialEq, Deserialize)]
+// struct StripeConfig { key: String, secret:String }
+
+fn cors_options() -> CorsOptions {
+    let allowed_origins = AllowedOrigins::All;
+    // let allowed_origins = AllowedOrigins::All;
+    // You can also deserialize this
+    rocket_cors::CorsOptions {
+        allowed_origins,
+        send_wildcard: true,
+        // allowed_methods: vec![Method::Get, Method::Post, Method::Options].into_iter().map(From::from).collect(),
+        allowed_headers: AllowedHeaders::All,
+        // allowed_headers: AllowedHeaders::some(&["Authorization", "Accept"]),
+        // allow_credentials: true,
+        ..Default::default()
+    }
+}
+#[tokio::main]
+async fn main() {
+    let figment = Config::figment();
+    let launch_result = rocket::custom(figment)
+        .mount(
+            "/",
+            openapi_get_routes![
+                // http_auth::http_auth,
+                // oauth2::oauth2_auth_code_get_user,
+                // open_id::open_id,
+                // cookies::cookie_auth,
+            ],
+        )
+        .mount(
+            "/",
+            routes![
+                api_key::session,
+            ],
+        )
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
+        )
+        .mount(
+            "/rapidoc/",
+            make_rapidoc(&RapiDocConfig {
+                general: GeneralConfig {
+                    spec_urls: vec![UrlObject::new("General", "../openapi.json")],
+                    ..Default::default()
+                },
+                ui: UiConfig {
+                    theme: Theme::Dark,
+                    ..Default::default()
+                },
+                hide_show: HideShowConfig {
+                    allow_spec_url_load: false,
+                    allow_spec_file_load: false,
+                    ..Default::default()
+                },
+                ..Default::default()
+            }),
+        )
+        .manage(cors_options().to_cors().expect("To not fail"))
+        .attach(Template::fairing())
+        .register(
+            "/",
+            catchers![bad_request, unauthorized, not_found, internal_error],
+        )
+        .launch()
+        .await;
+    match launch_result {
+        Ok(_) => println!("Rocket shut down gracefully."),
+        Err(err) => println!("Rocket had an error: {}", err),
+    };
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+// ----- Catchers -------
+
+/// Error messages returned to user
+#[derive(Debug, serde::Serialize, schemars::JsonSchema)]
+pub struct MyError {
+    /// The title of the error message
+    pub err: String,
+    /// The description of the error
+    pub msg: Option<String>,
+    // HTTP Status Code returned
+    #[serde(skip)]
+    pub http_status_code: u16,
+}
+
+#[catch(400)]
+fn bad_request() -> MyError {
+    MyError {
+        err: "Bad Request".to_owned(),
+        msg: Some("The request given is wrongly formatted or data was missing.".to_owned()),
+        http_status_code: 400,
+    }
+}
+#[catch(500)]
+fn internal_error() -> MyError {
+    MyError {
+        err: "Bad Request".to_owned(),
+        msg: Some("Whoops! Looks like we messed up.".to_owned()),
+        http_status_code: 500,
+    }
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> Template {
+    let default = LogFileConfig::new();
+    let mut logfile = LogFile::new(default).unwrap();
+    logfile.write(
+        LogLevel::INFO,
+        format!(
+            "origin_ip:{:?} | remote:{:?}",
+            req.headers().get_one("cf-connecting-ip"),
+            req.remote()
+                .into_iter()
+                .collect::<Vec<std::net::SocketAddr>>()
+        )
+        .as_str(),
+    );
+    let apikey = "PJkAekkIqocvtj5jMsc2CnGIs7ogOWcd";
+    let error_str = "The page you requested cannot be found right now";
+    Template::render("error", context! { apikey, error_str })
+}
+#[catch(401)]
+fn unauthorized() -> MyError {
+    MyError {
+        err: "Unauthorized".to_owned(),
+        msg: Some("The authentication given was incorrect or insufficient.".to_owned()),
+        http_status_code: 401,
+    }
+}
+
+/// Create my custom response
+///
+/// Putting this in a separate function somewhere will resolve issues like
+/// <https://github.com/GREsau/okapi/issues/57>
+pub fn bad_request_response(gen: &mut OpenApiGenerator) -> okapi::openapi3::Response {
+    let schema = gen.json_schema::<MyError>();
+    okapi::openapi3::Response {
+        description: "\
+        # 400 Bad Request\n\
+        The request given is wrongly formatted or data was missing. \
+        "
+        .to_owned(),
+        content: okapi::map! {
+            "application/json".to_owned() => MediaType {
+                schema: Some(schema),
+                ..Default::default()
+            }
+        },
+        ..Default::default()
+    }
+}
+
+pub fn unauthorized_response(gen: &mut OpenApiGenerator) -> okapi::openapi3::Response {
+    let schema = gen.json_schema::<MyError>();
+    okapi::openapi3::Response {
+        description: "\
+        # 401 Unauthorized\n\
+        The authentication given was incorrect or insufficient. \
+        "
+        .to_owned(),
+        content: okapi::map! {
+            "application/json".to_owned() => MediaType {
+                schema: Some(schema),
+                ..Default::default()
+            }
+        },
+        ..Default::default()
+    }
+}
+
+impl<'r> Responder<'r, 'static> for MyError {
+    fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
+        // Convert object to json
+        let body = serde_json::to_string(&self).unwrap();
+        Response::build()
+            .sized_body(body.len(), std::io::Cursor::new(body))
+            .header(rocket::http::ContentType::JSON)
+            .status(rocket::http::Status::new(self.http_status_code))
+            .ok()
+    }
+}
+
+impl OpenApiResponderInner for MyError {
+    fn responses(gen: &mut OpenApiGenerator) -> std::result::Result<Responses, OpenApiError> {
+        use okapi::openapi3::RefOr;
+        Ok(Responses {
+            responses: okapi::map! {
+                "400".to_owned() => RefOr::Object(bad_request_response(gen)),
+                "401".to_owned() => RefOr::Object(unauthorized_response(gen)),
+            },
+            ..Default::default()
+        })
+    }
 }
